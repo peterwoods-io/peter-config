5d5b5ed89a4c74659b8712150cfc1a9e1e504b5a
Peter Woods <peter@peterwoods.io>
2025-11-07 16:43:19 -0800

    Tidy up some old stuff, remove submodules, etc.

--- powerline-shell-customizations/segments/peter_exit_code.py
+++
@@ -1,9 +0,0 @@
-from ..utils import BasicSegment
-
-class Segment(BasicSegment):
-    def add_to_powerline(self):
-        if self.powerline.args.prev_error == 0:
-            return
-        fg = self.powerline.theme.CMD_FAILED_FG
-        bg = self.powerline.theme.CMD_FAILED_BG
-        self.powerline.append(u' \u2718', fg, bg, "")
